6ee600b6af91a0631897448018d6a327a235edd5
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 15:10:05 +0000

    Update MilkTank.java

--- MilkTank.java
+++ MilkTank.java
@@ -1,2 +1,39 @@
 public class MilkTank {
+
+    private int customCapacity;
+
+    public MilkTank() {
+    }
+
+    public MilkTank(int customCapacity) {
+        this.customCapacity = customCapacity;
+    }
+
+    public int getCustomCapacity() {
+        return customCapacity;
+    }
+
+    public double freeSpace(){
+        return 0;
+    }
+
+    public double addToTank(double amount){
+
+        return 0;
+
+    }
+
+    public double getFromTank(double amount){
+
+        return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return "MilkingMachine{" +
+                "customCapacity=" + customCapacity +
+                '}';
+    }
+    
 }
