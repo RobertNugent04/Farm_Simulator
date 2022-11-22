9b1ca8648c7da4eb8886c13508766bb5171c2a54
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 14:38:44 +0000

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Farm {
 
     public static int heaterId;
+    public static int idCount = 1;
 
     private String owner;
 
@@ -10,5 +11,46 @@ public class Farm {
 
     private ArrayList<Animal> animals;
 
+    public Farm() {
+    }
 
+    public Farm(String owner, Object shed, ArrayList<Animal> animals) {
+        this.owner = owner;
+        this.shed = shed;
+        this.animals = animals;
+        this.heaterId = idCount;
+        idCount++;
+    }
+
+    public static int getHeaterId() {
+        return heaterId;
+    }
+
+    public static void setHeaterId(int heaterId) {
+        Farm.heaterId = heaterId;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Object getShed() {
+        return shed;
+    }
+
+    public void setShed(Object shed) {
+        this.shed = shed;
+    }
+
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
 }
