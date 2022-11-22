120bfafd2caafd3ce5169d2c5a84120527809ffb
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 16:02:37 +0000

    Update MilkingMachine.java

--- MilkingMachine.java
+++ MilkingMachine.java
@@ -1,2 +1,16 @@
 public class MilkingMachine {
+
+private Object MilkTank;
+
+    public MilkingMachine() {
+    }
+
+    public Object getMilkTank() {
+        return MilkTank;
+    }
+
+    public void setMilkTank(Object milkTank) {
+        MilkTank = milkTank;
+    }
+    
 }
