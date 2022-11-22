e3782435ecdf2ae58c1c01a5af3328b0f6465bb4
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 14:34:35 +0000

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Farm {
+
+    public static int heaterId;
+
+    private String owner;
+
+    private Object shed;
+
+    private ArrayList<Animal> animals;
+
+
 }
