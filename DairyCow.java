82305a0707ed22bc461e8064375b7e1121221c64
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 15:58:01 +0000

    Update DairyCow.java

--- DairyCow.java
+++ DairyCow.java
@@ -1,2 +1,39 @@
-public class DairyCow {
+import java.util.Random;
+
+public class DairyCow extends Animal{
+
+    Random rand = new Random();
+
+    public static int dairyId;
+    public static int idCount = 1;
+
+    private String name;
+
+    private int udderCapacity;
+
+    public DairyCow() {
+    }
+
+    public DairyCow(String name) {
+        this.name = name;
+        this.udderCapacity = rand.nextInt(40 - 20) + 20;
+        this.dairyId = idCount;
+        idCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUdderCapacity() {
+        return udderCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "DairyCow{" +
+                "name='" + name + '\'' +
+                ", udderCapacity=" + udderCapacity +
+                '}';
+    }
 }
