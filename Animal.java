0cc1b4f530ebfeb3aad9fb64c0697fa355d38f32
Robert Nugent <117461435+RobertNugent04@users.noreply.github.com>
2022-11-22 15:00:31 +0000

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,5 +1,39 @@
 public abstract class Animal {
 
+    private double weight;
 
+    private String colour;
+
+    public Animal() {
+    }
+
+    public Animal(double weight, String colour) {
+        this.weight = weight;
+        this.colour = colour;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "weight=" + weight +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
 
 }
